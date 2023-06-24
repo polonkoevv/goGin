eadd2732e6bd4ca86ef94ab2437c56dacac014ca
bersnakx <87599231+polonkoevv@users.noreply.github.com>
2023-06-24 03:59:48 +0300

    Lesson 1 -- Simple REST API

---
+++ service/video-service.go
@@ -0,0 +1,25 @@
+package service
+
+import "github.com/polonkoevv/goGin/entity"
+
+type VideoService interface {
+	FindAll() []entity.Video
+	Save(entity.Video) entity.Video
+}
+
+type videoService struct {
+	videos []entity.Video
+}
+
+func New() VideoService {
+	return &videoService{}
+}
+
+func (service *videoService) FindAll() []entity.Video {
+	return service.videos
+}
+
+func (service *videoService) Save(video entity.Video) entity.Video {
+	service.videos = append(service.videos, video)
+	return video
+}
